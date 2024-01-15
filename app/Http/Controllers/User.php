f3c87dc776f5ed5b5cd366516b54350cf4c46a44
Mohammed Ibrahim <agentmame@gmail.com>
2024-01-15 21:56:32 +0300

    first commit

---
+++ app/Http/Controllers/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User as ModelsUser;
+use Illuminate\Http\Request;
+
+class User extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        // Fetch a user from the database using Eloquent
+        $user = ModelsUser::find($id);
+
+        if ($user) {
+            // User found, return user data
+            return response()->json($user);
+        } else {
+            // User not found, return an appropriate response
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
