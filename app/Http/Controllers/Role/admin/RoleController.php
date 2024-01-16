1f8bc533f8b1648abd62929d631a343dcaa6e843
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-01-16 22:06:11 +0300

    finished seeder with out authorization logic

--- app/Http/Controllers/Role/admin/RoleController.php
+++ app/Http/Controllers/Role/admin/RoleController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Role\admin;
 
 use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use App\Http\Controllers\Controller;
 class RoleController extends Controller
 {
       //
--- app/Http/Controllers/Role/user/RoleController.php
+++ app/Http/Controllers/Role/user/RoleController.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Role\user;
 
-use App\Http\Requests\StoreRoleRequest;
-use App\Http\Requests\UpdateRoleRequest;
 use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
 
 class RoleController extends Controller
 {
-   //
+    //
+
+
 }
