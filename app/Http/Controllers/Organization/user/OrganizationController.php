d81dd32a85687ef4b11e512d95560afed8bbd294
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-01-17 14:00:56 +0300

    finished user routes

--- app/Http/Controllers/Organization/user/OrganizationController.php
+++ app/Http/Controllers/Organization/user/OrganizationController.php
@@ -3,9 +3,58 @@
 namespace App\Http\Controllers\Organization\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\Organization;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class OrganizationController extends Controller
 {
     //
+
+    public function index()
+    {
+        // Add your logic for listing items
+        $data = Organization::all();
+
+        return response()->json(["message" => "Organization retrieved successfully", "data" => $data], 200);
+    }
+
+    public function create()
+    {
+        // Add your logic for displaying the create form
+    }
+
+    public function store(Request $request)
+    {
+        // Add your logic for storing a new item
+    }
+
+    public function show($id)
+    {
+        // Add your logic for displaying a single item
+        $data = Organization::find($id);
+
+        if(!$data){
+            return response()->json(["message: " => "Record not found"],404);
+        }
+        Log::info("Requested ID: $id");
+
+        return response()->json(["message" => "Organization retrieved successfully", "data" => $data], 200);
+    }
+
+    public function edit($id)
+    {
+        // Add your logic for displaying the edit form
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Add your logic for updating an item
+    }
+
+    public function destroy($id)
+    {
+        // Add your logic for deleting an item
+    }
+
 }
